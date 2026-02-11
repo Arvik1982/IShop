f9438911bc9f433de7178425b308061bece13fd7
Арсений <@email>
2026-02-11 12:27:48 +0300

    fix deploy

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/index.ts";
 import { HelmetProvider } from "react-helmet-async";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
+    <HashRouter>
       <Provider store={store}>
         <HelmetProvider>
           <App />
         </HelmetProvider>
       </Provider>
-    </BrowserRouter>
-  </React.StrictMode>
+    </HashRouter>
+    {/* </BrowserRouter> */}
+  </React.StrictMode>,
 );
